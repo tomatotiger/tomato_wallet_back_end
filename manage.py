7f86642bdaf77a4cbd6f2d9037d69a2eaf92fdf4
Judy <yanghuanghuang@gmail.com>
2019-04-05 16:44:45 -0700

    update tomato_wallet_env to tomato_wallet

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tomato_wallet_back_end.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tomato_wallet.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
