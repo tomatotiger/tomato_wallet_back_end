2d96a62cd92a5f2e6c8f4aa50078fa0643612514
Judy <yanghuanghuang@gmail.com>
2019-04-22 11:20:08 -0700

    add new app wallet and it's models. Also add the models to admin

---
+++ wallet/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    parent = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+    name = models.CharField(blank=False, max_length=100)
+
+
+def get_sentinel_category():
+    return Category.objects.get_or_create(name='default')[0]
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.SET(get_sentinel_category))
+    record_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
