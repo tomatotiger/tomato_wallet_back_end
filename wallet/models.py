c512608eb0160f6396f0f6ce3a93449464eb1232
Judy <yanghuanghuang@gmail.com>
2019-04-26 11:38:58 -0700

    change amount field to DecimalField, also add validate to it

--- wallet/models.py
+++ wallet/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -13,14 +17,21 @@ class Category(models.Model):
         return self.name
 
 
+@receiver(pre_save, sender=Category)
+def validate_unique(sender, instance, **kwargs):
+    # unique_together of parent, name and owner
+    if Category.objects.filter(owner=instance.owner, name=instance.name).exists():
+        raise ValidationError("Duplicate Category")
+
+
 def get_sentinel_category():
-    return Category.objects.get_or_create(name='default')[0]
+    return Category.objects.get_or_create(name='Default')[0]
 
 
 class Expense(models.Model):
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    amount = models.FloatField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET(get_sentinel_category))
-    record_time = models.DateTimeField()
+    record_time = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
